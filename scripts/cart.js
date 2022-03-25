168931834ab2c306ceb389a99ac99ff899658d70
MrSzasz <lugoleandro@hotmail.com>
2022-03-25 14:27:51 -0300

    zoom for objects in shop

--- scripts/cart.js
+++ scripts/cart.js
@@ -173,7 +173,14 @@ let buyButton = document.querySelector('.buyButton')
 buyButton.addEventListener('click', confirmationModal)
 
 function confirmationModal() {
-    new AWN().modal('<form class="containerBuy was-validated"><h1 class="text-center h4 h1Modal p-4">= Gracias por confiar en nosotros =</h1><p class="h5 text-center">Ingrese su mail a continuacion y se le contactara a la brevedad</p><input type="email" class="inputModal mt-3" placeholder="ejemplo@mail.com" required><button type="submit" class="button w-100 mt-3" id="doneButton">FINALIZAR</button><p class="text-center p-2">Haga click fuera de la ventana para cancelar</p></form>')
+    new AWN().modal(`
+    <form class="containerBuy was-validated">
+        <h1 class="text-center h4 h1Modal p-4">= Gracias por confiar en nosotros =</h1>
+        <p class="h5 text-center">Ingrese su mail a continuacion y se le contactara a la brevedad</p>
+        <input type="email" class="inputModal mt-3" placeholder="ejemplo@mail.com" required>
+        <button type="submit" class="button mt-3" id="doneButton">FINALIZAR</button>
+        <p class="text-center p-2 infoText">Haga click fuera de la ventana para cancelar</p>
+    </form>`)
 
     let done = document.querySelector('#doneButton').addEventListener('click', checkValid)
 
--- scripts/shop.js
+++ scripts/shop.js
@@ -16,7 +16,7 @@
  productList.forEach(obj => {
 
      let createCard = document.createElement('div');
-     createCard.classList.add('card', 'text-center');
+     createCard.classList.add('card', 'text-center', 'd-flex');
      createCard.setAttribute('data-inCart', false);
 
      // operador ternario
@@ -24,12 +24,11 @@
      obj.blessing ? createCard.classList.add('blessing') : createCard.classList.add('curse');
 
      createCard.innerHTML = `
-                     <img src="${obj.img}" class="card-img-top cardObjImg" alt="${obj.name}">
+                     <img src="${obj.img}" data-img-url-2="${obj.img2}" data-img-url-3="${obj.img3}" class="card-img-top cardObjImg" alt="${obj.name}">
                      <div class="card-body">
-                         <h2 class="card-title text-center cardObjName" data-curse="${obj.curse}" data-blessing="${obj.blessing}" data-condition-desc="${obj.conditionDesc}">${obj.name}</h2>
-                         <p class="card-text text-center cardObjDesc"></p>
-                         <p class="card-text text-center cardObjPrice">$${obj.price}.</p>
-                         <a class="btn button cardObjButton btn-primary">AGREGAR AL CARRITO</a>
+                         <h2 class="card-title text-center cardObjName flex-fill" data-curse="${obj.curse}" data-blessing="${obj.blessing}" data-condition-desc="${obj.conditionDesc}" data-condition-text="${obj.description}">${obj.name}</h2>
+                         <p class="card-text text-center cardObjPrice align-items-center flex-fill">$${obj.price}.</p>
+                         <a class="btn button cardObjButton btn-primary flex-fill">AGREGAR AL CARRITO</a>
                      </div>
  `;
 
@@ -40,6 +39,7 @@
  // =====  GET DATA FROM SHOP  =====
 
  let btnCart = document.querySelectorAll('.btn');
+
  btnCart.forEach(button => {
 
      button.addEventListener('click', (e) => {
@@ -80,3 +80,112 @@
          })
      });
  });
+
+
+ // ==========  MODAL FOR OBJECTS DATA ==========
+
+ let clickedImg = document.querySelectorAll('.cardObjImg')
+ clickedImg.forEach(card => {
+     card.addEventListener('click', (e) => {
+         clickedImg = e.target
+         let clickedImgContainer = clickedImg.closest('.card')
+         let objImg = clickedImgContainer.querySelector('.cardObjImg').src;
+         let objImg2 = clickedImgContainer.querySelector('.cardObjImg').getAttribute('data-img-url-2');
+         let objImg3 = clickedImgContainer.querySelector('.cardObjImg').getAttribute('data-img-url-3');
+         let objName = clickedImgContainer.querySelector('.cardObjName').textContent;
+         let objCurse = clickedImgContainer.querySelector('.cardObjName').getAttribute('data-curse');
+         let objBlessing = clickedImgContainer.querySelector('.cardObjName').getAttribute('data-blessing');
+         let objCondDesc = clickedImgContainer.querySelector('.cardObjName').getAttribute('data-condition-desc');
+         let objCondText = clickedImgContainer.querySelector('.cardObjName').getAttribute('data-condition-text');
+         let objPrice = Number(clickedImgContainer.querySelector('.cardObjPrice').textContent.replace('$', ''));
+
+         new AWN().modal(`<div class="infoOfObject">
+         <div class="containerImg d-flex align-items-center flex-column"><img class="text-center objModalImg" data-curse="${objCurse}" data-blessing="${objBlessing}" data-condition-desc="${objCondDesc}" data-img-url-2="${objImg2}" data-img-url-3="${objImg3}" src="${objImg}" alt="${objName}"><p class="text-center p-2 infoText">Haga click en la imagen para agrandar</p></div>
+         <div class="containerText">
+             <h1 class="text-center objModalName h3 mt-3">${objName}</h1>
+             <div class="textAndInfo d-flex flex-column">
+                 <p class="text-justify objModalDesc p-3">${objCondText}</p>
+                 <p class="objModalPrice px-3 text-start">$${objPrice}</p>
+             </div>
+         </div>
+         <div class="containerButton text-center m-3"><button class="button buttonForBuyInModal text-center">Agregar al carrito</button><p class="text-center p-2 infoText">Haga click fuera de la ventana para cerrar</p></div>
+     </div>`)
+
+         // ==========  BUY IN MODAL ==========
+
+         let buttonForBuyInModal = document.querySelector('.buttonForBuyInModal')
+         console.log(buttonForBuyInModal)
+         buttonForBuyInModal.addEventListener('click', (e) => {
+
+
+             let modalContainer = buttonForBuyInModal.closest('.infoOfObject');
+
+             let modalObjForBuyImg = modalContainer.querySelector('.objModalImg').src;
+             let modalObjForBuyCurse = modalContainer.querySelector('.objModalImg').getAttribute('data-curse');
+             let modalObjForBuyBlessing = modalContainer.querySelector('.objModalImg').getAttribute('data-blessing');
+             let modalObjForBuyCondDesc = modalContainer.querySelector('.objModalImg').getAttribute('data-condition-desc');
+             let modalObjForBuyName = modalContainer.querySelector('.objModalName').textContent;
+             let modalObjForBuyPrice = Number(modalContainer.querySelector('.objModalPrice').textContent.replace('$', ''));
+
+             let objectCart = {
+                 img: modalObjForBuyImg,
+                 name: modalObjForBuyName,
+                 curse: modalObjForBuyCurse,
+                 blessing: modalObjForBuyBlessing,
+                 conditionDesc: modalObjForBuyCondDesc,
+                 price: modalObjForBuyPrice,
+                 quantity: 1,
+             }
+
+             addThisElementToCart(objectCart);
+
+             // ==========  SAVE ON LOCAL STORAGE ==========
+
+             saveObjectsToCart();
+
+             // ==========  ALERT FOR CART ==========
+
+             notifier.success(`= ${objName} =`, {
+                 durations: {
+                     success: 3000
+                 }
+             })
+         })
+
+
+         // ==========  ZOOM IMG ==========
+
+         let imgForZoom = document.querySelector('.objModalImg')
+         imgForZoom.addEventListener('click', modalForImgZoom)
+
+         function modalForImgZoom() {
+             new AWN().modal(`<div id="carouselExampleIndicators" class="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
+             <div class="carousel-indicators">
+               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+             </div>
+             <div class="carousel-inner">
+               <div class="carousel-item active">
+                 <img src="${objImg}" class="d-block imgCarousel" alt="${objName}">
+               </div>
+               <div class="carousel-item">
+                 <img src="${objImg2}" class="d-block imgCarousel" alt="${objName}">
+               </div>
+               <div class="carousel-item">
+                 <img src="${objImg3}" class="d-block imgCarousel" alt="${objName}">
+               </div>
+             </div>
+             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+               <span class="visually-hidden">Previous</span>
+             </button>
+             <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+               <span class="carousel-control-next-icon" aria-hidden="true"></span>
+               <span class="visually-hidden">Next</span>
+             </button>
+           </div>`)
+         }
+         //  console.log(imgForZoom)
+     })
+ })
