cca649b3c7d75280475571999b2e1e61ba0f9fba
MrSzasz <lugoleandro@hotmail.com>
2022-03-26 12:16:42 -0300

    jquery added, preload status: done

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,30 @@
-window.onload = () =>{
-    let preloader = document.querySelector('.preloader');
-    preloader.classList.add('hide')
+window.onload = () => {
+    let screen = $(window).width()
+ 
+    console.log(screen)
+    let preloaderContainer = document.querySelector('.preloader');
+
+    anime({
+        targets: '.preloaderRight',
+        translateX: (screen+100),
+        easing: 'easeOutSine',
+        delay: 666,
+        duration: 6000,
+    })
+    anime({
+        targets: '.preloaderLeft',
+        translateX: -(screen+100),
+        easing: 'easeOutSine',
+        delay: 666,
+        duration: 6000,
+    })
+
+    let loadingSvg = document.querySelector('.preloaderSvg');
+    setTimeout(() => {
+        loadingSvg.classList.toggle('fade')
+    }, 400);
+
+    setTimeout(() => {
+        preloaderContainer.classList.add('hide')
+    }, 3000);
 }
